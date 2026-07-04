4e6dbf048da6afae3bb034145562ec38da89d471
MuhammadMuzammil-TheDeveloper <muzammil.thedeveloper@gmail.com>
2026-07-04 22:15:21 +0500

    File Management

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-console.log("Hello World")
+// const num1 = 100
+// const num2 = 100
+
+// console.log("Result", num1+num2)
+
+import fs, { read } from "fs";
+
+// const createFile = () => {
+//     fs.writeFileSync("name.txt", "Muhammad Muzammil")
+// }
+// createFile()
+
+
+// const readFile = (() => {
+//     fs.readFile("./name.txt", "utf-8", (err, success)=>{
+//         if(err){
+//             console.log(err)
+//         }else{
+//             console.log(success)
+//         }
+//     })
+// })()
+
+// const updateFile = (() =>{
+//     fs.appendFile('./name.txt', "\nMuhammad Ahmed", (err, success)=>{
+//         if(err){
+//             console.log(err)
+//         }else{
+//             console.log(success)
+//         }
+//     })
+// })()
+
+// const deleteFile = (()=>{
+//     fs.unlink('./name.txt', (err, success)=>{
+//         if(err){
+//             console.log(err)
+//         }
+//         else{
+//             console.log(success)
+//         }
+//     })
+// })()
