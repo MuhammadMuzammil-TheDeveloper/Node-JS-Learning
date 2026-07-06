f7877939342c32f30b1f63fde08b6bd1df83c3db
MuhammadMuzammil-TheDeveloper <muzammil.thedeveloper@gmail.com>
2026-07-06 13:54:10 +0500

    Learning Module

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 
 // console.log("Result", num1+num2)
 
-import fs, { read } from "fs";
+// import fs, { read } from "fs";
 
 // const createFile = () => {
 //     fs.writeFileSync("name.txt", "Muhammad Muzammil")
@@ -49,12 +49,21 @@ import fs, { read } from "fs";
 // // unlink = delete 
 
 
-const creatFolder =( () => {
-    fs.mkdir("./Cheking/check100/heelo.txt", (err, success)=>{
-        if(err){
-            console.log(err)
-        }else{
-            console.log(success)
-        }
-    })
-})()
+// const creatFolder =( () => {
+//     fs.mkdir("./Cheking/check100/heelo.txt", (err, success)=>{
+//         if(err){
+//             console.log(err)
+//         }else{
+//             console.log(success)
+//         }
+//     })
+// })()
+
+
+console.log("Its working....")
+
+
+
+const {add, sub} = require("./math")
+console.log("Math value is ",sub(10, 80))
+console.log("Math value is ",add(10, 80))
