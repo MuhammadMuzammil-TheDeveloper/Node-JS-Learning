3ff9b6ee597b6c30ef66632f74c9327a5f15282d
MuhammadMuzammil-TheDeveloper <muzammil.thedeveloper@gmail.com>
2026-07-13 22:53:36 +0500

    creating singup and login API

--- classWork/class_5/server.js
+++ classWork/class_5/server.js
@@ -1,4 +1,5 @@
 import express from "express"
+import fs from "fs"
 
 const app = express()
 const PORT = 5000
@@ -9,8 +10,17 @@ app.get('/', (req, res)=>{
 
 })
 app.post('/singup', (req, res)=>{
-    res.send("User Created!")
-    console.log("Data", req.body)
-    // console.log(req.body)
+    // res.send("User Created!")
+    // console.log("Data", req.body)
+    // // console.log(req.body)
+    const userObj = req.body
+    if(fs.existsSync("users.txt")){
+        const userData = JSON.parse()
+
+    }else{
+        fs.writeFileSync('user.txt', JSON.stringify([userObj]))
+        res.send("user Createddd!!")
+    }
+
 })
 app.listen(PORT, ()=> console.log("Server"))
